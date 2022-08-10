09052078555af5a0f1cbe9045e3a47a3da3cb712
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-10 14:26:00 -0400

    Implemented 2nd solution using a helper function and regex

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,31 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+
 function anagrams(stringA, stringB) {
-    const characterMapOne = buildCharMap(stringA);
-    const characterMapTwo = buildCharMap(stringB);
-
-    if (Object.keys(characterMapOne).length !== Object.keys(characterMapTwo).length) {
-        return false;
-    }
-
-    for (let char in characterMapOne) {
-        if (characterMapOne[char] !== characterMapTwo[char]) {
-            return false;
-        }
-    }
-
-    return true;
+    return cleanString(stringA) === cleanString(stringB);
 }
 
-function buildCharMap(str) {
-    const charMap = {};
-
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-        charMap[char] = charMap[char] + 1 || 1;
-    }
-
-    return charMap;
+function cleanString(str) {
+    return str
+        .replace(/[^\w]/g, "")
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join("");
 }
+
+
+// function anagrams(stringA, stringB) {
+//     const characterMapOne = buildCharMap(stringA);
+//     const characterMapTwo = buildCharMap(stringB);
+
+//     if (Object.keys(characterMapOne).length !== Object.keys(characterMapTwo).length) {
+//         return false;
+//     }
+
+//     for (let char in characterMapOne) {
+//         if (characterMapOne[char] !== characterMapTwo[char]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// function buildCharMap(str) {
+//     const charMap = {};
+
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+// }
 
 module.exports = anagrams;

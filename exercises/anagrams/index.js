c941af19ed3521cd643dbf23154edbf9d3c4d055
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-10 13:58:34 -0400

    Implemented solution with if/else statement and a helper function

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const characterMapOne = buildCharMap(stringA);
+    const characterMapTwo = buildCharMap(stringB);
+
+    if (Object.keys(characterMapOne).length !== Object.keys(characterMapTwo).length) {
+        return false;
+    }
+
+    for (let char in characterMapOne) {
+        if (characterMapOne[char] !== characterMapTwo[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
 
 module.exports = anagrams;

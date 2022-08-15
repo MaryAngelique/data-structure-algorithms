67bb2f6f9477d1be3bd5e32166a8f62877a487f8
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-15 11:20:46 -0400

    Changed the bubble sort to pass the test

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -6,12 +6,14 @@ function bubbleSort(arr) {
         for (let j = 0; j < (arr.length - i - 1); j++) {
             if (arr[j] > arr[j+1]) {
                 const lesser = arr[j+1];
+                
                 arr[j+1] = arr[j];
                 arr[j] = lesser;
             }
         }
-        return arr;
     }
+
+    return arr;
 }
 
 function selectionSort(arr) {

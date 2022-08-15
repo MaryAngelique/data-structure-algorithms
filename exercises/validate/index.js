5b28691252a8798b2fef2fe8a4dd8d2c69237b2d
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-15 12:30:05 -0400

    Implemented a min and max value

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -7,6 +7,13 @@
 
 function validate(node, min = null, max = null) {
     if (max !== null && node.data > max) {
+        return false;
+    }
+
+    if (min !== null && node.data < min) {
+        return false;
+    }
+
     
 }
 
